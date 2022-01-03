ac1d4b0b65ff115f7ab66ae28beaf0e70a4771aa
Yohei Shimmyo <qwpmb554@gmail.com>
2022-01-03 18:36:39 +0900

    add js for username registeration

--- web/html/synmodoro/static/assets/js/app.js
+++ web/html/synmodoro/static/assets/js/app.js
@@ -2,9 +2,13 @@ var timer = document.getElementById('timer');
 var type = document.getElementById('type');
 var type_button = document.getElementById('timerbutton');
 var notify_elemenet = document.getElementById('notify');
+var mates_element = document.getElementById('mates');
+var editusername_element = document.getElementById('edit-username');
+var username = randomUserID()Z;
 const wsurl = 'ws://' + window.location.host + '/api/ws';
 const stateuri = 'http://' + window.location.host + '/api/state';
 const starturi = 'http://' + window.location.host + '/api/start';
+const registeruri = 'http://' + window.location.host + '/api/register';
 var request = new XMLHttpRequest();
 
 function parseState(statestr){
@@ -23,6 +27,38 @@ function draw(work_status, endtime){
   return diff;
 }
 
+function randomUserID(){
+  let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let rand_str = '';
+  const len = 8;
+  for ( var i = 0; i < len; i++ ) {
+    rand_str += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+}
+
+function registerName(){
+  fetch(registeruri, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+    redirect: 'follow',
+    body: username,
+  }).then(response => {
+    const ok = response.status;
+    if(ok != 200){
+      const errmsg = await response.text();
+      console.log("Failed to register the username.");
+      console.log(errmsg);
+      showErrorMessage("Failed to register username: " + errmsg);
+    }
+  });
+
+}
+
 function onOpen(event){
   console.log("Connected.");
   notify_elemenet.classList.remove('active');
@@ -31,18 +67,34 @@ function onOpen(event){
 function  onMessage(event) {
   notify_elemenet.classList.remove('active');
   const timerinfo = JSON.parse(event.data);
-  const endtimeunixsec = timerinfo.EndTimeUnixSec;
-  const state = timerinfo.Status;
+  if (timerinfo.hasAttribute('timerinfo')){
+    const endtimeunixsec = timerinfo.timerinfo.EndTimeUnixSec;
+    const state = timerinfo.timerinfo.Status;
 
-  var interval = setInterval(function(){
-    const diff = draw(state, endtimeunixsec);
-    if(diff <= 0) {
-      clearInterval(interval);
-      const notification = new Notification("Finish " + state);
-      const sound = new Audio("/sound/chaim.mp3");
-      sound.play();
+    var interval = setInterval(function(){
+      const diff = draw(state, endtimeunixsec);
+      if(diff <= 0) {
+        clearInterval(interval);
+        const notification = new Notification("Finish " + state);
+        const sound = new Audio("/sound/chaim.mp3");
+        sound.play();
+      }
+    }, 1000);
+  }else if (timerinfo.hasAttribute('workingmates')){
+    const mates = timerinfo.workingmates.Mates;
+    mates_element.innerHTML = "<table>";
+    for (mate in elements) {
+      if (mate == username) {
+        mates_element.innerHTML = "<tr><td>" + mate + "<span id=\"edit-username\">&#9999</span></td></tr>";
+      }else{
+        mates_element.innerHTML = "<tr><td>" + mate + "</td></tr>";
+      }
     }
-  }, 1000);
+  }else{
+    console.log("Invalid websocket message.");
+    console.log(timerinfo);
+    showErrorMessage("Invalid websocket message: " + timerinfo);
+  }
 }
 
 function onClick() {
@@ -73,20 +125,42 @@ function onClick() {
 
 function showErrorMessage(message) {
   notify_elemenet.classList.toggle('active');
-  notify_elemenet.innerHTML = "Websocket connection error: " + message;
+  notify_elemenet.innerHTML += message;
 
 }
 
 function onError(event){
   console.log("WS Connection Error.");
   console.log(event);
-  showErrorMessage(event);
+  showErrorMessage("Websocket connection error: " + event);
 }
 
 function onClose(event){
   console.log("WS Connection Closed.");
   console.log(event);
-  showErrorMessage(event);
+  showErrorMessage("Websocket connection error: " + event);
+}
+
+function registerUsername(event){
+  fetch(registeruri, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+    redirect: 'follow',
+    body: username,
+  }).then(response => {
+    const ok = response.status;
+    if (ok != 200){
+      const errmsg = await response.text();
+      console.log("Failed to register username.");
+      console.log(errmsg);
+      showErrorMessage("Failed to register username: " + errmsg);
+    }
+  }
 }
 
 window.onload = function(){
@@ -109,6 +183,7 @@ window.onload = function(){
     connection.onclose = onClose;
 
     type_button.onclick = onClick;
+    editusername_element.onclick = registerUsername;
   });
 
   // Notification configuration
